748751df7f3b99cae5c636cefcfc5a6641dfab77
La Min Ko Ko <lamin.koko@techcombine.co>
2024-11-17 04:05:53 +0700

    fix build

--- client-site/src/app/(profile)/my-account/page.tsx
+++ client-site/src/app/(profile)/my-account/page.tsx
@@ -95,7 +95,6 @@ const AccountPage: React.FC = () => {
         return;
       }
 
-      // Perform the staking transaction
       const tx = await ecoTokenContract.stakeTokens(stakeAmountWei);
       await tx.wait();
       alert("Successfully staked!");
